aaefcb6ca3d15c3517ebdce45b30f0a79bc1eb57
Andreas Olofsson <andreas@zeroasic.com>
2025-04-26 18:00:46 -0400

    Adding Design object notes

--- logicbenchy/design.py
+++ logicbenchy/design.py
@@ -7,6 +7,36 @@ import siliconcompiler as sc
 # Usage: gcc [options] file...
 # Usage: clang [options] file...
 
+# Examples of types of design sources:
+#  1.) human generated software (verilog, vhdl, python, c)
+#  2.) ai generated software (verilog, vhdl, python, c)
+#  3.) deterministic generated code (chisel-->verilog for example)
+#  4.) schematics (block diagrams)
+#  5.) netlist (1:1 correlation with schematics)
+
+# Transpilers and modifiers allowed, C code-->C code
+# Veriog --> Verilog
+
+# Design --> SC --> Part --> SC --> Part
+
+# Source/schema should support syntax for files, pip, git, local
+# use the gcc command line options -L for the path to the library files and -l to link in a library (a .so or a .a):
+
+# options:
+# env?
+# var?
+# entrypoint, per fileset (CHANGE?)
+# idir
+# ydir
+# vlib
+# ldir? (CHANGE/ADD?)
+# define (equal sign or not?)
+# libext
+# param (make consistent with DEFINE)
+
+# tuples: (scribe, panelname, datasheet, constraint
+# tuples: constraint, flawgraph
+
 class Design(sc.Chip):
     def __init__(self,name):
         super().__init__(name)
@@ -34,6 +64,6 @@ class Design(sc.Chip):
         '''return list of filesets'''
         pass
 
-    def getfile(self, fileset, filetype=None):
+    def getsource(self, fileset, filetype=None):
         '''return files of type fileset'''
         pass
