8552b3e4bf8bb3692af57661a5ab8224afe1cb7a
Andreas Olofsson <andreas@zeroasic.com>
2025-06-24 14:13:53 -0400

    Implementing relu

--- logikbench/arithmetic/relu/rtl/relu.v
+++ logikbench/arithmetic/relu/rtl/relu.v
@@ -1,3 +1,9 @@
-module relu();
-//complete code
+module relu #(parameter N = 8) // data width
+   (
+    input signed [N-1:0]  in,
+    output signed [N-1:0] out
+    );
+
+   assign out = (in > 0) ? in : 0;
+
 endmodule
