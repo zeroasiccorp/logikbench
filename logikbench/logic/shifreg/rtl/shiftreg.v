2024af8c1f58daef62fefb0c2915c1bc6ff1cf32
Andreas Olofsson <andreas@zeroasic.com>
2025-06-24 13:57:08 -0400

    Cleaning up shifreg, starting over

--- logikbench/logic/shifreg/rtl/shiftreg.v
+++ logikbench/logic/shifreg/rtl/shiftreg.v
@@ -1,22 +1,5 @@
-module shiftreg #(parameter SW = 4  // selector width
-	          )
-   (
-    //Inputs
-    input [N-1:0]  a, // a input (multiplier)
-    input [N-1:0]  b, // b input (multiplicand)
-    //Outputs
-    output [M-1:0] z  // a * b final product
-    );
-parameter SELWIDTH = 5;
-input CLK, CE, SI;
-input [SELWIDTH-1:0] SEL;
-output DO;
-   localparam DW = 2**SELWIDTH;
-reg [DW-1:0] data;
-assign DO = data[SEL];
-always @(posedge CLK)
- begin
- if (CE == 1'b1)
- data <= {data[DATAWIDTH-2:0], SI};
- end
+module shiftreg ();
+
+// simple shift register
+
 endmodule
