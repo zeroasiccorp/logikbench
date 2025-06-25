4b9320ac4c07a845b00a23403ebfbc876b3fbf44
Andreas Olofsson <andreas@zeroasic.com>
2025-06-25 08:43:35 -0400

    Adding restriction on parameter

--- logikbench/logic/onehot/rtl/onehot.v
+++ logikbench/logic/onehot/rtl/onehot.v
@@ -1,3 +1,19 @@
-module onehot();
-//complete code
+module onehot #(parameter DW = 32 // must be power of 2!
+                )
+   (
+    input wire [$clog2(DW)-1:0] in, // binary input
+    output reg [DW-1:0]         out // onehot output
+    );
+
+
+   integer i;
+
+   always @(*) begin
+      out = {DW{1'b0}};
+      for (i = 0; i < DW; i = i + 1) begin
+         if (in == i[$clog2(DW)-1:0])
+           out[i] = 1'b1;
+      end
+   end
+
 endmodule
