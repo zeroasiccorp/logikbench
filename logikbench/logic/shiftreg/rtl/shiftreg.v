258925ae06495ab5545aa1780a42ada36d671179
Andreas Olofsson <andreas@zeroasic.com>
2025-06-25 13:03:33 -0400

    Adding shift register code

--- logikbench/logic/shiftreg/rtl/shiftreg.v
+++ logikbench/logic/shiftreg/rtl/shiftreg.v
@@ -1,3 +1,23 @@
-module shiftreg();
-//complete code
+module shiftreg # (parameter DW = 8
+                   )
+   (
+    input           clk,
+    input           en, // shift enable
+    input           in, // serial input
+    output [DW-1:0] out // parallel output
+    );
+
+   reg [DW-1:0] preg;
+   integer i;
+
+   always @(posedge clk)
+     if (en) begin
+        for (i = 0; i < DW-1; i = i+1)
+          preg[i+1] <= preg[i];
+        preg[0] <= in;
+     end
+
+   // exposing allof shift register
+   assign out[DW-1:0] = preg;
+
 endmodule
