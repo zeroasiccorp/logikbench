eb78ddad700980e1051df18204a5a91b39cc63be
Andreas Olofsson <andreas@zeroasic.com>
2025-06-25 17:12:55 -0400

    Adding bitmask ram, asic style

---
+++ logikbench/memory/rambit/rtl/rambit.v
@@ -0,0 +1,25 @@
+module rambit #(parameter DW = 32, // Memory width
+                parameter AW = 10  // Address width
+                )
+   (input               clk,  // write clock
+    input               ce,   // chip enable
+    input [DW-1:0]      we,   // per bit write mask
+    input [AW-1:0]      addr, // write address
+    input [DW-1:0]      din,  // write data
+    output reg [DW-1:0] dout  // read output data
+    );
+
+   // Generic RTL RAM
+   reg     [DW-1:0] ram[(2**AW)-1:0];
+   integer          i;
+
+   // ASIC style bitmask
+   always @(posedge clk)
+     if (ce) begin
+        for (i=0;i<DW;i=i+1)
+          if (we[i])
+            ram[addr[AW-1:0]][i] <= din[i];
+        dout[DW-1:0] <= ram[addr[AW-1:0]];
+     end
+
+endmodule
