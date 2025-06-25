6a6477749b721083d9c154ffdc5ea42b3f66764b
Andreas Olofsson <andreas@zeroasic.com>
2025-06-25 15:46:25 -0400

    Adding ram with enable signal - typical patern to latch the dout when not accessed

---
+++ logikbench/memory/ramspnc/rtl/ramspnc.v
@@ -0,0 +1,23 @@
+module ramspnc #(parameter DW = 32,
+               parameter AW = 6
+               )
+   (
+    input               clk,  // clock
+    input               en,   // memory enable
+    input               we,   // write enable
+    input [AW-1:0]      addr, // addr
+    input [DW-1:0]      din,  // data input
+    output reg [DW-1:0] dout  // data output
+    );
+
+   reg [DW-1:0] mem [(2**AW)-1:0];
+
+   always @(posedge clk) begin
+      if(en) begin
+         if (we)
+           mem[addr] <= din;
+         else
+           dout <= mem[addr];
+      end
+   end
+endmodule
