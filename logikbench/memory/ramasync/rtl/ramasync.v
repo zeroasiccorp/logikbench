c3f786c8b43fbd896223d9ec8c175326dde82bcd
Andreas Olofsson <andreas@zeroasic.com>
2025-06-25 15:56:31 -0400

    Implementing async read port ram

---
+++ logikbench/memory/ramasync/rtl/ramasync.v
@@ -0,0 +1,21 @@
+module ramdist #(parameter DW = 32,
+                 parameter AW = 6
+                 )
+   (
+    input           clk,  // clock
+    input           we,   // write enable
+    input [AW-1:0]  addr, // addr
+    input [DW-1:0]  din,  // data input
+    output [DW-1:0] dout  // data output
+    );
+
+   reg [DW-1:0] mem [(2**AW)-1:0];
+
+   always @(posedge clk) begin
+      if (we)
+        mem[addr] <= din;
+   end
+
+   assign dout[DW-1:0] <= mem[addr];
+
+endmodule
--- logikbench/memory/ramdist/rtl/ramdist.v
+++
@@ -1,3 +0,0 @@
-module ramdist();
-//complete code
-endmodule
