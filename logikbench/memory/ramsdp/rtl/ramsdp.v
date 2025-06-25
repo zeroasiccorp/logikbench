3fa2ae46f2701a938976d80cfb017cf9fe485cc1
Andreas Olofsson <andreas@zeroasic.com>
2025-06-25 15:53:56 -0400

    Fixing incorrect understanding of simple dual port

--- logikbench/memory/ramsdp/rtl/ramsdp.v
+++ logikbench/memory/ramsdp/rtl/ramsdp.v
@@ -1,32 +1,34 @@
 module ramsdp #(parameter DW = 32,
-                parameter AW= 6
+                parameter AW = 6
                 )
    (
     // single clock
     input               clk,
     // Port A - Read/Write
     input               we_a,
+    input               en_a,
     input [AW-1:0]      addr_a,
     input [DW-1:0]      din_a,
-    output reg [DW-1:0] dout_a,
     // Port B - Read Only
+    input               en_b,
     input [AW-1:0]      addr_b,
     output reg [DW-1:0] dout_b
     );
 
-    // Memory declaration
-    reg [DW-1:0] mem [(2**AW)-1:0];
+   // Memory declaration
+   reg [DW-1:0] mem [(2**AW)-1:0];
 
-   // Port A: Read/Write
+   // Port A: Write Only
    always @(posedge clk) begin
-      if (we_a)
-        mem[addr_a] <= din_a;
-      dout_a <= mem[addr_a];
+      if (en_a)
+        if (we_a)
+          mem[addr_a] <= din_a;
    end
 
    // Port B: Read Only
    always @(posedge clk) begin
-      dout_b <= mem[addr_b];
+      if (en_b)
+        dout_b <= mem[addr_b];
    end
 
 endmodule
