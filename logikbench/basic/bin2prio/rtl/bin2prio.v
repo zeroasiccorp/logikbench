bec7cbf81feb1cf5070469f204c12b9a3949ae13
Andreas Olofsson <andreas@zeroasic.com>
2025-06-26 12:14:46 -0400

    Module name change for clarity

---
+++ logikbench/basic/bin2prio/rtl/bin2prio.v
@@ -0,0 +1,26 @@
+module bin2prio #(parameter DW = 32) // data width
+   (
+    input [DW-1:0]      in,   // binary input vector
+    output reg [DW-1:0] out,  // one hot output
+    output reg          valid // input vector!=0
+    );
+
+
+   integer i;
+   reg     found;
+
+   always @(*) begin
+      out   = {DW{1'b0}};
+      valid = 0;
+      found = 0;
+
+      for (i = DW-1; i >= 0; i = i - 1) begin
+         if (!found && in[i]) begin
+            out[i] = 1'b1;
+            valid  = 1;
+            found  = 1;
+         end
+      end
+   end
+
+endmodule
