cc05efd6fe09c346b7ae15d3793e42007fc25908
Andreas Olofsson <andreas@zeroasic.com>
2025-06-27 09:02:35 -0400

    Implementing min/max functions

--- logikbench/arithmetic/max/rtl/max.v
+++ logikbench/arithmetic/max/rtl/max.v
@@ -1,3 +1,13 @@
-module max();
-//complete code
+module max #(parameter DW = 8  // Operator width (8,16,32,64,128,...)
+	         )
+   (
+    //Inputs
+    input [DW-1:0]  a,
+    input [DW-1:0]  b,
+    //Outputs
+    output [DW-1:0] z
+    );
+
+   assign z = (a > b) ? a : b;
+
 endmodule
--- logikbench/arithmetic/min/rtl/min.v
+++ logikbench/arithmetic/min/rtl/min.v
@@ -1,3 +1,13 @@
-module min();
-//complete code
+module min #(parameter DW = 8  // Operator width (8,16,32,64,128,...)
+	         )
+   (
+    //Inputs
+    input [DW-1:0]  a,
+    input [DW-1:0]  b,
+    //Outputs
+    output [DW-1:0] z
+    );
+
+   assign z = (a < b) ? a : b;
+
 endmodule
