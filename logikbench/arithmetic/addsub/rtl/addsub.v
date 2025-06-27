1011b18a7f6e7940bc1ce531e821eefe1b72127c
Andreas Olofsson <andreas@zeroasic.com>
2025-06-27 09:06:01 -0400

    Implementing sub and selectable addsub

--- logikbench/arithmetic/addsub/rtl/addsub.v
+++ logikbench/arithmetic/addsub/rtl/addsub.v
@@ -1,3 +1,14 @@
-module addsub();
-//complete code
+module addsub #(parameter N = 8  // Operator width (8,16,32,64,128,...)
+	     )
+   (
+    //Inputs
+    input [N-1:0]  a,
+    input [N-1:0]  b,
+    input          sel,
+    //Outputs
+    output [N-1:0] z
+    );
+
+   assign z = sel ? a + b : a - b;
+
 endmodule
--- logikbench/arithmetic/sub/rtl/sub.v
+++ logikbench/arithmetic/sub/rtl/sub.v
@@ -1,3 +1,13 @@
-module sub();
-//complete code
+module sub #(parameter N = 8  // Operator width (8,16,32,64,128,...)
+	     )
+   (
+    //Inputs
+    input [N-1:0]  a,
+    input [N-1:0]  b,
+    //Outputs
+    output [N-1:0] z
+    );
+
+   assign {cout, z[N-1:0]} = a[N-1:0] - b[N-1:0];
+
 endmodule
