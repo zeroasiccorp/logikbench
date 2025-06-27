a8f5c00de0e76d68c2fbdd2c874fb3888aecc04b
Andreas Olofsson <andreas@zeroasic.com>
2025-06-27 09:04:27 -0400

    Implement inc and dec functions

---
+++ logikbench/arithmetic/dec/rtl/dec.v
@@ -0,0 +1,11 @@
+module dec #(parameter N = 8  // Operator width (8,16,32,64,128,...)
+	     )
+   (
+    //Inputs
+    input [N-1:0]  a,
+    output [N-1:0] z
+    );
+
+   assign z = a - 1'b1;
+
+endmodule
