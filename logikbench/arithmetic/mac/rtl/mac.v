bbb1582bc719914d53ef0763d72d7e152345f99a
Andreas Olofsson <andreas@zeroasic.com>
2025-06-27 09:05:38 -0400

    Implementing basic multiply accumulate circuit

--- logikbench/arithmetic/mac/rtl/mac.v
+++ logikbench/arithmetic/mac/rtl/mac.v
@@ -1,3 +1,20 @@
-module mac();
-//complete code
+module mac #(parameter DW = 16,  // input data widths
+             parameter ACCW = 40 // accumulator width
+             )
+   (
+    input                 clk,
+    input                 clear,
+    input                 en,
+    input [DW-1:0]        a,
+    input [DW-1:0]        b,
+    output reg [ACCW-1:0] acc
+    );
+
+   always @(posedge clk) begin
+      if (clear)
+        acc <= 0;
+      else if (en)
+        acc <= acc + a * b;
+   end
+
 endmodule
