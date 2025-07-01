17abf41a1f0b7ec6864d2fd0c7af8f05c6bdb97c
Andreas Olofsson <andreas@zeroasic.com>
2025-07-01 14:29:03 -0400

    Refactoring to reduce typing...

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,90 +1,17 @@
-from logikbench import *
+import common
 
 def test_basic_setup():
-    for item in [Arbiter(),
-                 Band(),
-                 Bbuf(),
-                 Bin2gray(),
-                 Bin2prio(),
-                 Binv(),
-                 Bnand(),
-                 Bnor(),
-                 Bor(),
-                 Bxnor(),
-                 Bxor(),
-                 Crossbar(),
-                 Dffasync(),
-                 Dffsync(),
-                 Gray2bin(),
-                 Mux(),
-                 Muxcase(),
-                 Muxhot(),
-                 Muxpri(),
-                 Onehot(),
-                 Pipeline(),
-                 Shiftreg()]:
+    for item in common.basic_list:
         assert len(item.get_file('rtl')) == 1
 
 def test_memory_setup():
-    for item in [Cache(),
-                 Fifoasync(),
-                 Fifosync(),
-                 Ramasync(),
-                 Rambit(),
-                 Rambyte(),
-                 Ramdp(),
-                 Ramsdp(),
-                 Ramsp(),
-                 Ramspnc(),
-                 Regfile(),
-                 Rom()]:
+    for item in common.memory_list:
         assert len(item.get_file('rtl')) == 1
 
 def test_arithmetic_setup():
-
-    for item in [Abs(),
-                 Absdiff(),
-                 Absdiffs(),
-                 Add(),
-                 Addsub(),
-                 Cmp(),
-                 Counter(),
-                 Csa32(),
-                 Csa42(),
-                 Dec(),
-                 Dotprod(),
-                 Inc(),
-                 Log2(),
-                 Mac(),
-                 Max(),
-                 Min(),
-                 Mul(),
-                 Muladd(),
-                 Muladdc(),
-                 Mulc(),
-                 Mulreg(),
-                 Muls(),
-                 Relu(),
-                 Round(),
-                 Shiftar(),
-                 Shiftb(),
-                 Shiftl(),
-                 Shiftr(),
-                 Sine(),
-                 Sqdiff(),
-                 Sqrt(),
-                 Sub(),
-                 Sum()]:
+    for item in common.arithmetic_list:
         assert len(item.get_file('rtl')) == 1
 
 def test_block_setup():
-    for item in [Aes(),
-                 Axicrossbar(),
-                 Ethmac(),
-                 Ialu(),
-                 I2c(),
-                 Lfsr(),
-                 Picorv32(),
-                 Serv(),
-                 Uart()]:
+    for item in common.block_list:
         assert item.get_file('rtl')
