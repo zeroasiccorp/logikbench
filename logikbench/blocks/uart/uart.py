6faaa0a664cbc911d2a460ae28e62c0fbdf4792d
Andreas Olofsson <andreas@zeroasic.com>
2025-07-01 13:58:59 -0400

    Updating uart to new design and new Design object

--- logikbench/blocks/uart/uart.py
+++ logikbench/blocks/uart/uart.py
@@ -1,23 +1,37 @@
-#!/usr/bin/env python3
+from os.path import dirname, abspath
+from siliconcompiler.design import DesignSchema
 
-import os
-from siliconcompiler import Chip
 
-def setup():
+class Uart(DesignSchema):
+    def __init__(self):
 
-    # root dir
-    root = os.path.dirname(__file__)
+        name = 'uart'
+        root = f'{name}_root'
+        source = ['rtl/la_dsync.v',
+                  'rtl/uart_raminfr.v',
+                  'rtl/uart_receiver.v',
+                  'rtl/uart_rfifo.v',
+                  'rtl/uart_tfifo.v',
+                  'rtl/uart_transmitter.v',
+                  'rtl/uart.v',]
 
-    # library object
-    chip = Chip('uart')
+        # create a Design object
+        super().__init__(name)
 
-    # rtl
-    for item in ('uart.v',
-                 'uart_tx.v',
-                 'uart_rx.v'):
-        chip.input(os.path.join(root, 'rtl', item))
+        # set data home directory
+        self.register_package(root, dirname(abspath(__file__)))
 
-    # sdc
-    chip.input(os.path.join(root, 'sdc', 'uart.sdc'))
+        # rtl files
+        fileset = 'rtl'
+        for item in source:
+            self.add_file(item, fileset, package=root)
 
-    return chip
+        # include files
+        self.add_idir('rtl', fileset, package=root)
+
+        # top module
+        self.set_topmodule(name, fileset)
+
+if __name__ == "__main__":
+   d = Uart()
+   d.write_fileset(f"uart.f", fileset="rtl")
