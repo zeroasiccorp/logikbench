f733969188df9897835c5fced8a650821e9ac8a5
Andreas Olofsson <andreas@zeroasic.com>
2025-07-01 22:42:43 -0400

    Using yosys-slang plugin, works like a charm!

--- tests/test_yosys.py
+++ tests/test_yosys.py
@@ -6,7 +6,7 @@ def run_yosys(dlist):
 
     yosys_template =  """
 # Read the Verilog source file
-read_verilog_file_list -f {{ cmdfile }}
+read_slang -f {{ cmdfile }} --top {{ topmodule }}
 
 # Set the top module
 hierarchy -top {{ topmodule }}
@@ -27,33 +27,32 @@ write_verilog {{ netlist }}
         name = item.name()
         cmdfile = f'{name}.f'
         netlist =  f'{name}.vg'
-        # create .f cmd file
+
+        # create -f cmd file
         item.write_fileset(cmdfile, fileset='rtl')
         topmodule = item.get_topmodule(fileset='rtl')
 
-        # clean up cmd file (yosys bug)
-        cleanfile = cmdfile + ".clean"
-
-        with open(cmdfile, "r") as f_in, open(cleanfile, "w") as f_out:
-            for line in f_in:
-                if "//" not in line.strip():
-                    f_out.write(line)
-
-        # create yosys script
+        # create yosys script from template
         context = {
             'topmodule': topmodule,
-            'cmdfile': cleanfile,
+            'cmdfile': cmdfile,
             'netlist' : netlist
         }
         output = template.render(context)
         script = f"{name}.ys"
         with open(script, 'w') as f:
             f.write(output)
-        # get results
-        result = subprocess.run(["yosys", "-s", script],
+
+        # run yosys
+        cmd = ["yosys",
+               "-m", "slang",
+               "-s", script]
+        print(' '.join(cmd))
+        result = subprocess.run(cmd,
                                 check=True,
                                 capture_output=True,
                                 text=True)
+
         #grab stats here if you like
         #print(result)
 
@@ -63,5 +62,8 @@ def test_yosys_basic():
 def test_yosys_memory():
     run_yosys(common.memory_list)
 
-def test_icarus_arithmetic():
+def test_yosys_arithmetic():
     run_yosys(common.arithmetic_list)
+
+def test_yosys_blocks():
+    run_yosys(common.block_list)
