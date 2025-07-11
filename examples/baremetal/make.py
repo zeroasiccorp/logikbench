e8c49f896d4fc23272a29b80b24ebb073ee4d799
Andreas Olofsson <andreas@zeroasic.com>
2025-07-11 19:01:11 -0400

    Making benchmark pseudo dynamic

--- examples/baremetal/make.py
+++ examples/baremetal/make.py
@@ -5,11 +5,12 @@
 import argparse
 import subprocess
 import os
+import sys
 from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
+from logikbench import *
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(description="""\
 
 LogikBench bare metal usage example
@@ -19,18 +20,22 @@ Example Usage:
 make.py -tool yosys -group epfl
 """, formatter_class=argparse.RawDescriptionHelpFormatter)
 
-
-    parser.add_argument("-tool",
-                        choices=['yosys', 'vivado'],
-                        required=True,
-                        help="Synthesis tool")
     parser.add_argument("-group",
-                       nargs='+',
+                        nargs='+',
                         required=True,
                         help="Benchmark group")
     parser.add_argument("-name",
                         nargs='+',
                         help="Benchmark name")
+    parser.add_argument("-tool",
+                        choices=['yosys', 'vivado'],
+                        required=True,
+                        help="Benchmark tool")
+    parser.add_argument("-target",
+                        choices=['ice40'],
+                        required=True,
+                        help="Benchmark target")
+
 
     args = parser.parse_args()
 
@@ -42,39 +47,61 @@ make.py -tool yosys -group epfl
     env = Environment(loader=FileSystemLoader('.'))
     template = env.get_template(f'{args.tool}_template.j2')
 
-    # generic runtime script
-    script = "run.tcl"
-
     # iterate over all groups
     for group in args.group:
         group_path = rootdir / "logikbench" / group
         if args.name:
-           bench_list =args.name
+            bench_list = args.name
         else:
-           bench_list = [p.name for p in group_path.iterdir() if p.is_dir()]
+            bench_list = [p.name for p in group_path.iterdir() if p.is_dir()]
+
         # iterate over all benchmarks in group
         for name in bench_list:
+            if args.tool == 'yosys':
+                script = f"{name}.ys" # yosys corner case
+            else:
+                script = f"{name}.tcl"
 
-           os.makedirs(f"build/{group}/{name}", exist_ok=True)
-           os.chdir(f"build/{group}/{name}")
+            # get top module
+            # TODO: find module by string?
+            # TODO: generalize
+            mod = sys.modules["logikbench.basic.binv.binv"]
+            cls = getattr(mod, "Binv")
+            d = cls()
+            print(d.get_topmodule(fileset='rtl'))
 
-           # create tool script
-           output = template.render(context)
-           with open(script, 'w') as f:
-               f.write(output)
+            mod = importlib.import_module("logikbench.basic.binv.binv")
+            cls = getattr(mod, "Binv")
+            d = cls()
+            print(d.get_topmodule(fileset='rtl'))
 
-           # run tool
+            # create run dir
+            os.makedirs(f"build/{group}/{name}", exist_ok=True)
+            os.chdir(f"build/{group}/{name}")
+            filename = group_path / name / "rtl" / f"{name}.v"
+
+            # create tool script
+            context = {
+                'name': name,
+                'top': topmodule,
+                'filename': filename,
+                'target': args.target,
+            }
+            output = template.render(context)
+            with open(script, 'w') as f:
+                f.write(output)
+
+            # run benchmark
             try:
-             result = subprocess.run(
-                ["yosys", script],
-                check=True,
-                capture_output=True,
-                text=True)
-             print("Yosys output:")
-             print(result.stdout)
+                print(f"Running {name} benchmark in group '{group}'. Lofile: build/{group}/{name}/{name}.log")
+                with open(f'{name}.log', "w") as log:
+                          result = subprocess.run([args.tool, script],
+                                        stdout=log,
+                                        stderr=subprocess.STDOUT,
+                                        check=True)
 
-          except subprocess.CalledProcessError as e:
-             print("Yosys failed with error:")
-             print(e.stderr)
-             # go back home
-             os.chdir(scriptdir)
+            except subprocess.CalledProcessError as e:
+                print(f"Error...see logfile!!")
+
+            # go back home
+            os.chdir(scriptdir)
