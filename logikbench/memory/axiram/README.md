24d831d4cce9c8a3750da31211a600637c0e8145
Andreas Olofsson <andreas@zeroasic.com>
2025-07-13 07:36:07 -0400

    Adding single port axi ram

---
+++ logikbench/memory/axiram/README.md
@@ -0,0 +1,5 @@
+ -axilite single port SRAM
+
+d274c73cb72fbfd95eb879ae6aa666606c182a6d
+
+https://github.com/alexforencich/verilog-axi
---
+++ logikbench/memory/axiram/axiram.py
@@ -0,0 +1,30 @@
+from os.path import dirname, abspath
+from siliconcompiler.design import DesignSchema
+
+
+class Axiram(DesignSchema):
+    def __init__(self):
+
+        name = 'axiram'
+        root = f'{name}_root'
+        topmodule = 'axil_ram'
+        source = [f'rtl/axil_ram.v']
+
+        # create a Design object
+        super().__init__(name)
+
+        # set data home directory
+        self.register_package(root, dirname(abspath(__file__)))
+
+        # rtl files
+        fileset = 'rtl'
+        for item in source:
+            self.add_file(item, fileset, package=root)
+
+        # top module
+        self.set_topmodule(topmodule, fileset)
+
+
+if __name__ == "__main__":
+    d = Axiram()
+    d.write_fileset(f"{d.name()}.f", fileset="rtl")
