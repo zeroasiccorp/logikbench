0d563129c4eb65d842014841d18aa2305513812d
Rice Shelley <rice.shelley@ieee.org>
2025-07-31 22:51:44 -0400

    WIP

--- examples/sc/make.py
+++ examples/sc/make.py
@@ -9,26 +9,81 @@ from pathlib import Path
 from siliconcompiler.flows import fpgaflow
 from siliconcompiler import Chip
 from logikbench import *
+from typing import List, Any
+from dataclasses import dataclass
 
 
-def peter_benchmarks():
-    benchmarks = []
+@dataclass
+class BenchmarkMetaData:
+    group: str
+    name: str
+    bench_obj: Any
+    synth_results: Any = None
+
+
+def write_dict_as_csv(filename, data):
+    # writing results to file
+    with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        # Write header
+        writer.writerow(list(data[0].keys()))
+        # Write each row
+        for result in data:
+            writer.writerow(list(result.values()))
+
+
+def filter_for_benchmarks_peter_cares_about(
+        benchmarks: List[BenchmarkMetaData]) -> List[BenchmarkMetaData]:
+    benchmark_names = []
     with open("z1060_synth_only_logikbench_metrics_baseline_6lut.csv") as file:
         reader = csv.DictReader(file)
         for row in reader:
-            benchmarks.append(row["benchmark"])
+            benchmark_names.append(row["benchmark"])
+
+    return [b for b in benchmarks if b.name in benchmark_names]
+
+
+def load_benchmark(group, name):
+    bench = None
+    try:
+        # instance of benchmark class
+        mod = sys.modules[f"logikbench.{group}.{name}.{name}"]
+        bobj = getattr(mod, name.capitalize())
+        bench = bobj()
+        return bench
+    except KeyError:
+        return None
+
+
+def load_benchmark_group(group: str) -> List[BenchmarkMetaData]:
+
+    rootdir = Path(__file__).resolve().parent.parent.parent
+
+    benchmarks = []
+    group_path = rootdir / "logikbench" / group
+    bench_list = sorted([p.name for p in group_path.iterdir() if p.is_dir()])
+
+    # iterate over all benchmarks in group
+    for name in bench_list:
+        bench = load_benchmark(group, name)
+        if bench:
+            benchmarks.append(BenchmarkMetaData(
+                group=group,
+                name=name,
+                bench_obj=bench
+            ))
     return benchmarks
 
 
-def run(args):
-    # Error checking
-    if args.tool == 'vivado' and args.target is None:
-        print("Target must be specified with Vivado tool.")
-        exit()
+def load_all_benchmarks() -> List[BenchmarkMetaData]:
+    groups = ["arithmetic", "basic", "blocks", "epfl", "memory"]
+    benchmarks = []
+    for group in groups:
+        benchmarks.extend(load_benchmark_group(group))
+    return benchmarks
 
-    # resolving relative path
-    rootdir = Path(__file__).resolve().parent.parent.parent
 
+def peter_run():
     try:
         os.mkdir("build")
     except OSError:
@@ -39,102 +94,141 @@ def run(args):
     except OSError:
         pass
 
-    # global analysis
-    results = {}
+    pool = ThreadPool(processes=15)
 
-    rslt_queue = Queue()
+    benchmarks = load_all_benchmarks()
+
+    benchmarks = filter_for_benchmarks_peter_cares_about(benchmarks)
+
+    for synth_directive in ["AreaOptimized_high", "PerformanceOptimized"]:
+        results = [result.synth_results for result in pool.starmap(
+            synth_vivado,
+            [(bench, synth_directive) for bench in benchmarks]
+        )]
+        write_dict_as_csv(f"{synth_directive}_results.csv", results)
+
+
+def make_dirs():
+    dirs = [
+        "build",
+        "build/cmdfile_dir",
+        "build/verilog_wrapper_dir",
+    ]
+    for d in dirs:
+        try:
+            os.mkdir(d)
+        except OSError:
+            pass
+
+
+def thierry_run():
+    make_dirs()
 
     pool = ThreadPool(processes=15)
 
-    process_args = []
-
-    # Get all groups
-    for group in args.group:
-        group_path = rootdir / "logikbench" / group
-        if args.name:
-            bench_list = args.name
-        else:
-            bench_list = sorted([p.name for p in group_path.iterdir() if p.is_dir()])
-
-        # iterate over all benchmarks in group
-        for name in bench_list:
-            try:
-                # instance of benchmark class
-                mod = sys.modules[f"logikbench.{group}.{name}.{name}"]
-                bobj = getattr(mod, name.capitalize())
-                b = bobj()
-
-                # TODO: switch to thread pool
-                process_args.append((b, name, group, rslt_queue))
-            except KeyError:
-                pass
-
-    pb = peter_benchmarks()
-    process_args = [arg for arg in process_args if arg[1] in pb]
-    print(process_args)
-    assert (sorted([arg[1] for arg in process_args]) == sorted(pb)), "Missing benchmark that peter ran"
-
-    for result in pool.starmap(run_synth, process_args):
-        pass
-
-    results = [rslt_queue.get() for _ in range(rslt_queue.qsize())]
-    results = sorted(results, key=lambda r: r["group"] + r["name"] + r["syn_directive"])
-
-    # writing results to file
-    _, ext = os.path.splitext(args.output)
-    if ext == ".json":
-        with open(args.output, "w") as f:
-            json.dump(results, f, indent=2)
-    elif ext == ".csv":
-        with open(args.output, "w", newline="") as f:
-            writer = csv.writer(f)
-            # Write header
-            writer.writerow(list(results[0].keys()))
-            # Write each row
-            for result in results:
-                writer.writerow(list(result.values()))
+    for group in ["arithmetic"]:
+        benchmarks = load_benchmark_group(group)
+        for synth_directive in ["AreaOptimized_high", "PerformanceOptimized"]:
+            results = [result.synth_results for result in pool.starmap(
+                synth_vivado,
+                [(bench, synth_directive) for bench in benchmarks]
+            )]
+            write_dict_as_csv(f"{group}_{synth_directive}_results.csv", results)
 
 
-def run_synth(bench, name, group, rslt_queue):
+def rice_run():
+    make_dirs()
+
+    pool = ThreadPool(processes=15)
+
+    for group in ["basic"]:
+        benchmarks = load_benchmark_group(group)
+        for synth_directive in ["AreaOptimized_high", "PerformanceOptimized"]:
+            results = [result.synth_results for result in pool.starmap(
+                synth_vivado,
+                [(bench, synth_directive) for bench in benchmarks]
+            )]
+            write_dict_as_csv(f"{group}_{synth_directive}_results.csv", results)
+
+
+def aes():
+    make_dirs()
+
+    pool = ThreadPool(processes=15)
+
+    for group in ["blocks"]:
+        benchmarks = load_benchmark_group(group)
+        for synth_directive in ["AreaOptimized_high"]:
+            results = [result.synth_results for result in pool.starmap(
+                synth_vivado,
+                [(bench, synth_directive) for bench in benchmarks if bench.name == "aes"]
+            )]
+            write_dict_as_csv(f"{group}_{synth_directive}_results.csv", results)
+
+
+def synth_vivado(benchmark: BenchmarkMetaData, synth_directive: str):
     # get top module
-    topmodule = bench.get_topmodule(fileset='rtl')
+    topmodule = benchmark.bench_obj.get_topmodule(fileset='rtl')
 
     # write out design fileset
-    cmdfile = f"build/cmdfile_dir/{group}_{name}.f"
-    bench.write_fileset(cmdfile, fileset='rtl')
+    cmdfile = f"build/cmdfile_dir/{benchmark.group}_{benchmark.name}.f"
+    benchmark.bench_obj.write_fileset(cmdfile, fileset='rtl')
 
-    chip = Chip(topmodule)
+    top_wrapper_module_name = f"{benchmark.group}_{benchmark.name}_{synth_directive}_top"
+    top_wrapper_path = f"build/verilog_wrapper_dir/{top_wrapper_module_name}.v"
+    wrapper = f"""
+        module {top_wrapper_module_name}();
+            (* DONT_TOUCH = "TRUE" *)
+            {topmodule} {topmodule}_inst();
+        endmodule
+
+    """
+
+    with open(top_wrapper_path, "w") as f:
+        f.write(wrapper)
+
+    chip = Chip(top_wrapper_module_name)
+
     chip.import_flist(cmdfile)
+    chip.input("synth_const.xdc")
+    chip.input(top_wrapper_path)
+
     chip.use(fpgaflow, fpgaflow_type="vivado", partname="xc7a100tcsg324-1")
     chip.set("fpga", "partname", "xc7a100tcsg324-1")
     chip.set('option', 'flow', 'fpgaflow')
     chip.set('option', 'to', 'syn_fpga')
 
-    synth_directives = ["AreaOptimized_high", "PerformanceOptimized"]
-
-    for synth_directive in synth_directives:
-        chip.set('option', 'jobname', group + "_" + name + "_" + synth_directive)
-        chip.set('tool', 'vivado', 'task', 'syn_fpga', 'var', 'synth_directive', synth_directive)
-        chip.dashboard(type="cli")
-        assert chip.run()
-
+    chip.set('option', 'jobname', benchmark.group + "_" + benchmark.name + "_" + synth_directive)
+    chip.set('tool', 'vivado', 'task', 'syn_fpga', 'var', 'synth_directive', synth_directive)
+    chip.dashboard(type="cli")
+    if chip.run():
         luts = chip.get("metric", "luts", step='syn_fpga', index=0)
         regs = chip.get("metric", "registers", step='syn_fpga', index=0)
         brams = chip.get("metric", "brams", step='syn_fpga', index=0)
         dsps = chip.get("metric", "dsps", step='syn_fpga', index=0)
         cpu = chip.get("metric", "exetime", step='syn_fpga', index=0)
 
-        rslt_queue.put({
-            "group": group,
-            "name": name,
-            "syn_directive": synth_directive,
+        benchmark.synth_results = {
+            "name": benchmark.name,
             "luts": luts if luts else 0,
             "regs": regs if regs else 0,
             "dsps": dsps if dsps else 0,
             "brams": brams if brams else 0,
             "maxlvl": 0,
             "cpu": cpu
-        })
+        }
+    else:
+        benchmark.synth_results = {
+            "name": benchmark.name,
+            "luts": 0,
+            "regs": 0,
+            "dsps": 0,
+            "brams": 0,
+            "maxlvl": 0,
+            "cpu": 0
+        }
+
+    return benchmark
 
 
 if __name__ == "__main__":
@@ -145,7 +239,7 @@ LogikBench bare metal usage example
 -No dependency on external run time infrastructures
 
 Example Usage:
-python make.py -tool yosys -target ice40 -group  epfl -name mem_ctrl
+python make.py -target xc7a100tcsg423-1 -group basic -name arbiter
 
 """, formatter_class=argparse.RawDescriptionHelpFormatter)
 
@@ -161,18 +255,11 @@ python make.py -tool yosys -target ice40 -group  epfl -name mem_ctrl
     parser.add_argument("-name", "-n",
                         nargs='+',
                         help="Benchmark name")
-    parser.add_argument("-tool",
-                        choices=['yosys', 'vivado'],
-                        default="yosys",
-                        help="Tool name")
     parser.add_argument("-target",
                         help="Compilation target")
-    parser.add_argument('-clean', '-c',
-                        action='store_true',
-                        help='Clean up build directory')
     parser.add_argument('-output', '-o',
-                        default="build/results.json",
+                        default="build/results.csv",
                         help='Output file name')
 
     args = parser.parse_args()
-    run(args)
+    run(None, None)
---
+++ examples/sc/synth_const.xdc
@@ -0,0 +1,4 @@
+set_property BLOCK_SYNTH.MAX_LUT_INPUT 4 [get_cells *]
+set_property BLOCK_SYNTH.MUXF_MAPPING 0 [get_cells *]
+set_property BLOCK_SYNTH.ADDER_THRESHOLD 128 [get_cells *]
+set_property BLOCK_SYNTH.COMPARATOR_THRESHOLD 128 [get_cells *]
