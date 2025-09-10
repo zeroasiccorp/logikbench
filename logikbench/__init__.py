76b03a45584c9f38618e9686a58b3a77b6776c74
Andreas Olofsson <andreas@zeroasic.com>
2025-09-10 15:29:55 -0400

    Fixing pip install issue

--- logikbench/__init__.py
+++ logikbench/__init__.py
@@ -2,15 +2,12 @@
 # LogikBench version number
 __version__ = "0.0.1"
 
-# Benchmark modules
+# Sub-modules
 from logikbench import basic
 from logikbench import arithmetic
 from logikbench import memory
 from logikbench import blocks
 from logikbench import epfl
-
-
-
 
 __all__ = [
     "basic",
