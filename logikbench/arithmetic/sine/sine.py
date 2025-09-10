8121c1ee756aa8df4ed116433fb3749b584893fc
Andreas Olofsson <andreas@zeroasic.com>
2025-09-10 17:06:22 -0400

    Keeping sine table generation

--- logikbench/arithmetic/sine/sine.py
+++ logikbench/arithmetic/sine/sine.py
@@ -1,5 +1,24 @@
 from os.path import dirname, abspath
 from siliconcompiler.design import DesignSchema
+import math
+
+def sinetable(N):
+
+    # create table
+    AMP = N/2-1 # amplitude
+    OFFSET = N/2 # shift to make all values positive
+    values = []
+    for i in range(N):
+        angle = 2 * math.pi * i / N
+        val = int(round(AMP * math.sin(angle) + OFFSET))
+        values.append(val)
+
+    # print as localparam with reversed order
+    print(f"localparam [{int(math.log2(N))}*{N}-1:0] SINETABLE_{N} = {{")
+    for i, v in enumerate(reversed(values)):  # reversed so addr=0 is first
+        sep = "," if i != N-1 else ""
+        print(f"    {int(math.log2(N))}'d{v}{sep}")
+    print("};")
 
 
 class Sine(DesignSchema):
@@ -30,3 +49,4 @@ class Sine(DesignSchema):
 if __name__ == "__main__":
     d = Sine()
     d.write_fileset("sine.f", fileset="rtl")
+    #sinetable(256)
