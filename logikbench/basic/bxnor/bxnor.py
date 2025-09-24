c2808a645995fac50792f4877e29d089fae56273
Andreas Olofsson <andreas@zeroasic.com>
2025-09-24 15:32:09 -0400

    Playing around with template

--- logikbench/basic/bxnor/bxnor.py
+++ logikbench/basic/bxnor/bxnor.py
@@ -4,24 +4,11 @@ from siliconcompiler.design import DesignSchema
 
 class Bxnor(DesignSchema):
     def __init__(self):
-
         name = 'bxnor'
-        root = f'{name}_root'
-        source = [f'rtl/{name}.v']
-
-        # create a Design object
+        root = dirname(abspath(__file__))
         super().__init__(name)
-
-        # set data home directory
-        self.set_dataroot(root, dirname(abspath(__file__)))
-
-        # rtl files
-        fileset = 'rtl'
-        for item in source:
-            self.add_file(item, fileset, dataroot=root)
-
-        # top module
-        self.set_topmodule(name, fileset)
+        self.add_file(f"{root}/rtl/{name}.v", 'rtl')
+        self.set_topmodule(name, 'rtl')
 
 
 if __name__ == "__main__":
