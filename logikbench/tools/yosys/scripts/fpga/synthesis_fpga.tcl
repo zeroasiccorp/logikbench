f7bc9e774b53253117945b6fdb696bfeaafdad33
Andreas Olofsson <andreas@zeroasic.com>
2026-06-16 18:55:07 -0400

    Creating common synthesis scripting framework

---
+++ logikbench/tools/yosys/scripts/fpga/synthesis_fpga.tcl
@@ -0,0 +1,32 @@
+# Map the FPGA target string to a yosys synth command. lb keeps the list of
+# targets decoded here (see logikbench/flows/runner.py FPGA_TARGETS); a target
+# not in that list selects the ASIC flow instead.
+#
+# synth_microchip
+# synth_fabulous
+# synth_gatemate
+# synth_gowin
+# synth_ice40
+# synth_xilinx
+# synth_efinix
+# synth_achronix
+# synth_quicklogic
+# synth_intel
+# zeroasic --> synth_fpga + plugin wildebeest
+
+switch -- $sc_target {
+    microchip   { set synth_cmd synth_microchip }
+    fabulous    { set synth_cmd synth_fabulous }
+    gatemate    { set synth_cmd synth_gatemate }
+    gowin       { set synth_cmd synth_gowin }
+    ice40       { set synth_cmd synth_ice40 }
+    xilinx      { set synth_cmd synth_xilinx }
+    efinix      { set synth_cmd synth_efinix }
+    achronix    { set synth_cmd synth_achronix }
+    quicklogic  { set synth_cmd synth_quicklogic }
+    intel       { set synth_cmd synth_intel }
+    default     { yosys plugin -i wildebeest; set synth_cmd synth_fpga }
+}
+
+# options from lb are appended verbatim as arguments to the synth command
+yosys $synth_cmd -top $sc_topmodule {*}$sc_options
---
+++ logikbench/tools/yosys/scripts/synthesis.tcl
@@ -0,0 +1,54 @@
+###############################
+# Reading SC Schema
+###############################
+
+source ./sc_manifest.tcl
+yosys echo on
+
+set sc_refdir [sc_cfg_tool_task_get refdir]
+set fileset [lindex [sc_cfg_get option fileset] 0]
+
+set sc_rtl [concat \
+    [sc_cfg_get_fileset $sc_designlib $fileset systemverilog] \
+    [sc_cfg_get_fileset $sc_designlib $fileset verilog]]
+set sc_mode [sc_cfg_tool_task_get var mode]
+set sc_target [sc_cfg_tool_task_get var target]
+set sc_options [sc_cfg_tool_task_get var options]
+set sc_liberty [sc_cfg_tool_task_get var liberty]
+
+###############################
+# Read in Design
+################################
+
+yosys plugin -i slang
+yosys read_slang --top $sc_topmodule {*}$sc_rtl
+yosys hierarchy -check -top $sc_topmodule
+
+###############################
+# Synthesis Logic
+###############################
+
+# select the per-mode synthesis core: <refdir>/<mode>/synthesis_<mode>.tcl
+set script "$sc_refdir/$sc_mode/synthesis_$sc_mode.tcl"
+source $script
+
+########################################################
+# Record Stats
+########################################################
+
+# liberty (ASIC only) lets 'stat' report cell area
+set stat_libs {}
+if {$sc_liberty ne ""} {
+    set stat_libs [list -liberty $sc_liberty]
+}
+
+# turn off echo to prevent the stat command from showing up in the json file
+yosys echo off
+yosys tee -o ./reports/stat.json stat -json -top $sc_topmodule {*}$stat_libs
+yosys echo on
+
+########################################################
+# Write Outputs
+########################################################
+yosys write_verilog -noexpr -nohex -nodec "outputs/${sc_topmodule}.vg"
+yosys write_json "outputs/${sc_topmodule}.netlist.json"
