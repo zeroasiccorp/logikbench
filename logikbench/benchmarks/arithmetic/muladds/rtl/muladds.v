56a12c71b9e0ca2199674126b6832d43ff15faa1
Andreas Olofsson <andreas@zeroasic.com>
2026-07-07 13:58:39 -0400

    Adding signed muladd (without reg)

---
+++ logikbench/benchmarks/arithmetic/muladds/rtl/muladds.v
@@ -0,0 +1,20 @@
+//#############################################################################
+// Copyright: Zero ASIC. All rights Reserved.
+// Author: Andreas Olofsson
+// License:  MIT (see LICENSE file in LogikBench repository)
+//#############################################################################
+module muladds #(parameter DW = 16,
+                 parameter OW = 32
+                 )
+   (
+    //Inputs
+    input signed [DW-1:0]  a,  // a input (multiplier)
+    input signed [DW-1:0]  b,  // b input (multiplicand)
+    input signed [OW-1:0]  c,  // c input (add input)
+    //Outputs
+    output signed [OW-1:0] out // a * b + c
+    );
+
+   assign out = a * b + c;
+
+endmodule
