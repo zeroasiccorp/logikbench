357def74d157b0d5b4dad9de9bcb0aba137eacbf
Andreas Olofsson <andreas@zeroasic.com>
2026-07-19 08:15:30 -0400

    paramater name for consistency

--- logikbench/benchmarks/blocks/wordalign/rtl/wordalign.v
+++ logikbench/benchmarks/blocks/wordalign/rtl/wordalign.v
@@ -12,43 +12,43 @@
 // (comma at the low bits).
 //
 //#############################################################################
-module wordalign #(parameter	      W = 40,  // word width
+module wordalign #(parameter	      DW = 40,  // word width
                    parameter	      PW = 10, // pattern width
                    parameter [PW-1:0] COMMA = 10'b0011111010
                    )
    (
-    input		       clk,
-    input		       nreset,
-    input [W-1:0]	       din,    // deserialized word
-    output reg [W-1:0]	       dout,   // aligned word
-    output reg		       locked, // comma offset acquired
-    output reg [$clog2(W)-1:0] offset  // locked bit offset
+    input                       clk,
+    input                       nreset,
+    input [DW-1:0]              din,    // deserialized word
+    output reg [DW-1:0]         dout,   // aligned word
+    output reg                  locked, // comma offset acquired
+    output reg [$clog2(DW)-1:0] offset  // locked bit offset
     );
 
-   localparam OW = $clog2(W);
+   localparam OW = $clog2(DW);
 
    // sliding 2-word window (previous | current)
-   reg [W-1:0] prev;
-   wire [2*W-1:0] window = {din, prev};
+   reg [DW-1:0] prev;
+   wire [2*DW-1:0] window = {din, prev};
 
    // one comparator per candidate offset (generated)
-   wire [W-1:0]	  match;
+   wire [DW-1:0]   match;
    genvar	  o;
    generate
-      for (o = 0; o < W; o = o + 1) begin : g_match
+      for (o = 0; o < DW; o = o + 1) begin : g_match
          assign match[o] = (window[o +: PW] == COMMA);
       end
    endgenerate
 
    // isolate the lowest matching offset and encode to binary (one-hot -> bin)
-   wire [W-1:0]  first = match & (~match + 1'b1);
+   wire [DW-1:0]  first = match & (~match + 1'b1);
    wire		 found = |match;
    wire [OW-1:0] off_c;
    genvar	 b, j;
    generate
       for (b = 0; b < OW; b = b + 1) begin : g_enc
-         wire [W-1:0] contrib;
-         for (j = 0; j < W; j = j + 1) begin : g_bit
+         wire [DW-1:0] contrib;
+         for (j = 0; j < DW; j = j + 1) begin : g_bit
             assign contrib[j] = first[j] & j[b];
          end
          assign off_c[b] = |contrib;
@@ -57,13 +57,13 @@ module wordalign #(parameter	      W = 40,  // word width
 
    // align using the locked offset once acquired, else the current candidate
    wire [OW-1:0]  use_off = locked ? offset : off_c;
-   wire [2*W-1:0] rotated = window >> use_off;
-   wire [W-1:0]	  dout_nxt = rotated[W-1:0];
+   wire [2*DW-1:0] rotated = window >> use_off;
+   wire [DW-1:0]   dout_nxt = rotated[DW-1:0];
 
    always @(posedge clk or negedge nreset)
      if (!nreset) begin
-        prev   <= {W{1'b0}};
-        dout   <= {W{1'b0}};
+        prev   <= {DW{1'b0}};
+        dout   <= {DW{1'b0}};
         locked <= 1'b0;
         offset <= {OW{1'b0}};
      end
